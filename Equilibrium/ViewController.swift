609b02c3525908c39c71fd5219f8511da7e441fe
Shomil Jain <shomil@shomils-macbook-pro.local>
2017-06-10 15:16:10 -0700

    Initial Commit

---
+++ Equilibrium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Equilibrium
+//
+//  Created by Shomil Jain on 6/10/17.
+//  Copyright © 2017 Vayu Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
